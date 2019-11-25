63304120cf5b3561cdf9e7e8b518041da26b0259
Pebaz <pebazium@gmail.com>
2019-11-25 16:12:41 -0500

    Fixed it

--- member.py
+++ member.py
@@ -1,4 +1,4 @@
-import sys, subprocess
+import sys, os, subprocess
 
 def main():
 	if len(sys.argv) < 3:
@@ -9,14 +9,18 @@ def main():
 	_, seconds, message = sys.argv
 
 	command = (
-		f'import time, tkinter.messagebox; '
+		f'import time, os, tkinter.messagebox; '
 		f'time.sleep({seconds}); '
 		f'root = tkinter.Tk(); '
 		f'root.withdraw(); '
 		f'root.lift(); '
 		f'root.attributes("-topmost", True); '
-		f'tkinter.messagebox.showwarning("Member?", {repr(message)})'
 	)
+
+	if sys.platform == 'darwin':
+		command += """os.system('''/usr/bin/osascript -e 'tell app "Finder" to set frontmost of process "Python" to true' '''); """
+
+	command += f'tkinter.messagebox.showwarning("Member?", {repr(message)})'
 
 	python = 'python' if sys.platform == 'win32' else 'python3'
 	subprocess.Popen([python, '-c', command])
